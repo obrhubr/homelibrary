dbf7fa2a8ddee9dc8623c7f576a8c19e2ef80ef9
Iridium77Ir <osmiumos74@gmail.com>
2021-08-03 18:04:14 +0200

    change monitoring setup

--- src/back/main.js
+++ src/back/main.js
@@ -15,7 +15,18 @@ app.use(bodyParser.urlencoded({ limit: '15mb', extended: false }))
 // Create public folder to expose .epub and images to the world
 app.use(express.static('public'))
 
-const metricsMiddleware = promBundle({ includeMethod: true, includePath: true, includeStatusCode: true, customLabels: {project_name: 'nodejs'} });
+const metricsMiddleware = promBundle({ 
+    includeMethod: true, 
+    includePath: true, 
+    includeStatusCode: true, 
+    customLabels: {project_name: 'nodejs'},
+    normalizePath: [
+        ['^/search/simple/.*', '/search/simple/#searchquery'],
+        ['^/search/search-as-you-type/.*', '/search/search-as-you-type/#searchquery'],
+        ['^/search/books/one/.*', '/search/books/one/#searchquery'],
+        ['^/search/books/all/.*', '/search/books/all/#searchquery'],
+    ],
+});
 app.use(metricsMiddleware);
 
 app.all('*', function(req, res, next){
